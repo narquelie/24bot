60d1374d23da7945bbc6ac509096f42828b1ed06
narquelie <narquelie@mail.ru>
2022-12-31 16:41:21 +0300

    Update bot.js

--- JS/bot.js
+++ JS/bot.js
@@ -4,9 +4,9 @@ console.log('Starting...')
 
 function createBot () {
     const bot = mineflayer.createBot({
-    host: "localhost",
-    port: "25565",
-    username: "Test",
+    host: "narquelie.aternos.me",
+    port: "51427",
+    username: "Guard",
     version: false
     })
     bot.on('login', function() {
